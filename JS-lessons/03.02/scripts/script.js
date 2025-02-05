edffbf741182267d570965597a28acd9417fa4d3
Vasya Motrya <motravasa5@gmail.com>
2025-02-05 23:55:22 +0700

    ready registration form and form for send message

--- JS-lessons/03.02/scripts/script.js
+++ JS-lessons/03.02/scripts/script.js
@@ -10,11 +10,14 @@ document.addEventListener("DOMContentLoaded", function(){
     let flipCards = [];
     let moviesC = 0;
     let timerC = 0;
+    let timerInterval = null;
 
     btn.addEventListener("click", startGame);
     function startGame(){
         const size = parseInt(choice.value);
         createBoard(size);
+        resetTimer();
+        startTimer();
     }
     function createBoard(size){
         board.innerHTML = '';
@@ -26,7 +29,8 @@ document.addEventListener("DOMContentLoaded", function(){
         timer.innerHTML = timerC;
         const totalCards = size*size;
         const numbers = [];
-        for(let i=0; i<=totalCards/2; i++){
+        for(let i=1; i<=totalCards/2; i++){
+            numbers.push(i);
             numbers.push(i);
         }
         const pairs = cards.concat(numbers); // обьединение массивов
@@ -35,8 +39,56 @@ document.addEventListener("DOMContentLoaded", function(){
             const card = document.createElement('div');
             card.classList.add('card');
             card.dataset.number = pairs[i];
+            card.addEventListener('click', flipCard);
             board.appendChild(card);
             cards.push(card);
         }
+    }
+    function flipCard(){
+        
+        if(flipCards.length < 2 && this.classList.contains('flipped')) return;
+        this.classList.add('flipped');
+        this.textContent = this.dataset.number;
+        flipCards.push(this);
+        if(flipCards.length === 2){
+            moviesC++;
+            movies.innerHTML = moviesC;
+            setTimeout(checkMatch, 500);
+        }
+    }
+    function checkMatch(){
+        const c1 = flipCards[0];
+        const c2 = flipCards[1];
+        if(c1.dataset.number === c2.dataset.number){
+            c1.classList.add('match');
+            c2.classList.add('match');
+            flipCards = [];
+        
+            if (document.querySelectorAll('.match').length === cards.length){
+                clearInterval(timerInterval);
+                alert(`Вы выиграли! Игра длилась ${timerC}, вы сделали ${moviesC} ходов`);
+            }
+        }else{
+            c1.classList.remove('flipped');
+            c2.classList.remove('flipped');
+            c1.textContent = '';
+            c2.textContent = '';
+            flipCards = [];
+        }
+    }
+    function startTimer(){
+        clearInterval(timerInterval)
+        timerC = 0; 
+        timer.innerHTML = timerC;
+        timerInterval = setInterval(() => {
+            timerC++;
+            timer.innerHTML = timerC;
+        }, 1000);
+    }
+    function resetTimer(){
+        clearInterval(timerInterval)
+        timerC = 0;
+        timer.innerHTML = timerC;
+
     }
 })
